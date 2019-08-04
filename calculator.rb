34d21b4260eb721d53f4abd35a104e0bf10c7c3e
Oksana Nichols <oksanahnichols@yahoo.com>
2019-08-04 21:28:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,5 @@
-# Add your variables here
+# Add`spec/calculator_spec.rb`. Hey, there's something! What's all of
+#this stuff doing?
+#your variables here
+
+#WRITE YOUR CODE HERE!! Well..below this line
